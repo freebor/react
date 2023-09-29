7399ca29a86853c4aeb97d469adbd6e9c072e2bf
Freedom Chisom <chisomnwanoruo6@gmail.com>
2023-09-29 04:39:49 -0700

    not yet responsive

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./assets/component/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/assets/component/Header/TopHeader.jsx
@@ -0,0 +1,29 @@
+
+const TopHeader = ()=>{
+    return (
+        <div className="top-header df">
+            <div className="num-email df">
+                <div className="number df">
+                    <i class="fa-solid fa-phone"></i>
+                    <a href="#">(225) 556-0118</a>
+                </div>
+                <div className="email df">
+                    <i class="fa-regular fa-envelope"></i>
+                    <a href="chisomnwanoruo6@gmail.com">chisomnwanoruo6@gmail.com</a>
+                </div>
+            </div>
+            <div className="follow-text">
+                <p>follow us to get a chance to win 80% off</p>
+            </div>
+            <div className="follow-icon df">
+                <p>Follow Us :</p>
+                <a href=""><i class="fa-brands fa-instagram"></i></a>
+                <a href=""><i class="fa-brands fa-youtube"></i></a>
+                <a href=""><i class="fa-brands fa-facebook"></i></a>
+                <a href=""><i class="fa-brands fa-twitter"></i></a>
+            </div>
+        </div>
+    )
+}
+
+export default TopHeader
