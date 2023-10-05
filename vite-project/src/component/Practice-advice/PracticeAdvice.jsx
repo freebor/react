57ad678cb734a6fb4b79145e5b430329ca3ee463
Freedom Chisom <chisomnwanoruo6@gmail.com>
2023-10-05 11:59:13 -0700

    almost done

---
+++ vite-project/src/component/Practice-advice/PracticeAdvice.jsx
@@ -0,0 +1,20 @@
+const PracticeAdvice = ()=>{
+    return(
+        <section className="featured-container-wrap">
+            <div className="PracticeAdvice-container">
+                <div className="PracticeAdvice-header">
+                    <h6>Practice Advice</h6>
+                    <h3>Featured Posts</h3>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default PracticeAdvice
+
+{/* <i class="fa-solid fa-star"></i> */}
+{/* <i class="fa-solid fa-heart"></i> */}
